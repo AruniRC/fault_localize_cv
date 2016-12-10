e8b7908928a22ea13722ed7503c35a7c7a23b1b9
Jong-Chyi Su <jcfredsu@gmail.com>
2016-12-10 17:17:56 -0500

    fixed the bug, now have cross validation results to csv files

--- src/cv_scores_basic.py
+++ src/cv_scores_basic.py
@@ -6,11 +6,7 @@ import random
 
 '''
 	Basic skeleton code to create folds and display values.
-
-
 '''
-
-
 
 
 FILENAME = '../data/scores_artificial_vs_real_with_indicator.csv' # Path to fault_localization_data/.../
@@ -18,7 +14,7 @@ N_FOLDS = 5
 
 # read in the CSV file as a Pandas DataFrame
 df = pd.read_csv(FILENAME)
-print(df.head())  # debug: check if the values look ok
+#print(df.head())  # debug: check if the values look ok
 
 # Handle non-unique Family values:
 # 	The column "Formula" has the value "ochiai" repeated for the same Bug,
@@ -34,19 +30,13 @@ df.loc[(df.Formula == 'ochiai') & (df.Family == 'mbfl'), 'Formula'] \
 # projectSet = list(set(df['Project'].tolist()))
 projectSet = df.Project.unique()
 df['Fold'] = np.nan
+cvMeanScore = []
 for fs in projectSet:
+	
 	# select all other projects - Leave One Project Out
-	print(fs)
-	# idx = (df['Project'] != fs).tolist() 
-	# print(len(idx))
-	df.loc[ (~(df['Project'] == fs)) , 'Fold'] = fs # DOESN'TT WORK!!!!!
-
-import pdb
-pdb.set_trace()
-
-# mean scores for each project
-cvMeanScore = df.groupby(['Fold', 'isReal', 'Formula'])['ScoreWRTLoadedClasses'].describe()
-
-
-
-
+	df['Fold'] = np.nan
+	df.at[df['Project'] != fs,'Fold'] = fs
+	
+	# mean scores for each project
+	cvMeanScore = df.groupby(['Fold', 'isReal', 'Formula'])['ScoreWRTLoadedClasses'].describe()
+	cvMeanScore.to_csv('results/cv_result'+fs+'.csv', sep=',', header=True, index=True)
